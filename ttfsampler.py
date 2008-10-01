f04f25ca826226f00179b8064aada3203eb05531
Dwayne C. Litzenberger <dlitz@dlitz.net>
2008-09-30 21:54:25 -0400

    Version 0.3

--- ttfsampler.py
+++ ttfsampler.py
@@ -17,7 +17,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ###########################################################################
 
-__version__ = "0.2"
+__version__ = "0.3"
 __revision__ = "$Id$"
 
 import sys
