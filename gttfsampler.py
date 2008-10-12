3a8d28fefcd20a36ac9b42c8ece0bd6fdf55ef8c
Dwayne C. Litzenberger <dlitz@dlitz.net>
2008-10-11 22:46:32 -0400

    GUI: Finish gttfsampler

--- gttfsampler.py
+++ gttfsampler.py
@@ -23,16 +23,108 @@ __revision__ = "$Id$"
 
 import Tkinter as T
 import tkFileDialog
+import tkMessageBox
 
 import os
 import re
 import fnmatch
+import threading
+import Queue
+
+ttfsampler = None   # This will be imported when the user choses "Save PDF..."
 
 def pack_widget(_w, **kw):
     _w.pack(**kw)
     return _w
 
 FILE_EXTENSIONS = "*.ttf *.otf *.TTF *.OTF" # List of file exstensions. (NB: tcl substitution will happen on this string.)
+
+class GUILog(object):
+    def __init__(self, config, queue):
+        self.cfg = config
+        self.queue = queue
+
+    def debug(self, msg):
+        self.queue.put(self._parse(msg))
+
+    def warning(self, msg):
+        self.queue.put(('W', self._parse(msg)[1]))
+
+    def error(self, msg):
+        self.queue.put(('E', self._parse(msg)[1]))
+
+    def _parse(self, msg):
+        if msg.startswith("<") and msg[2:3] == '>':
+            return (int(msg[1]), msg[3:])
+        else:
+            return (0, msg)
+
+
+class BatchThread(threading.Thread):
+    def __init__(self, config, listbox):
+        threading.Thread.__init__(self, None, None, self.__class__.__name__)
+        self.cfg = config
+        self.listbox = listbox
+        self.queue = Queue.Queue()
+        self.log = GUILog(self.cfg, self.queue)
+        self.core = ttfsampler.TTFSampler(self.cfg, self.log)
+        self.finished = False
+        self.error = None
+
+    def run(self):
+        try:
+            try:
+                self.core.run()
+            except ttfsampler.error, exc:
+                self.error = str(exc)
+            except Exception, exc:
+                self.error = str(exc)
+                raise
+            else:
+                self.error = None
+        finally:
+            self.queue.put((1, "Finished."))
+            self.finished = True
+
+    def process_queue(self):
+        assert threading.currentThread() is not self
+
+        # Check if the window is scrolled to the bottom
+
+        if self.listbox.yview()[1] == 1.0:
+            scroll_to_bottom = True
+        else:
+            scroll_to_bottom = False
+
+        try:
+            while True:
+                (loglevel, msg) = self.queue.get_nowait()
+                i = self.listbox.index("end")
+                if loglevel == 'E':
+                    self.listbox.insert(i, "Error: " + msg)
+                    self.listbox.itemconfigure(i, background="pink")
+                elif loglevel == 'W':
+                    self.listbox.insert(i, "Warning: " + msg)
+                    self.listbox.itemconfigure(i, background="light yellow")
+                elif self.cfg.verbosity >= loglevel:
+                    self.listbox.insert(i, msg)
+        except Queue.Empty:
+            pass
+
+        # Scroll the window to the bottom, if that's where the user wants it.
+        if scroll_to_bottom:
+            self.listbox.yview("moveto", 1.0)
+
+        if self.finished:
+            if self.error is None:
+                msgbox = tkMessageBox.Message(title="gTTFSampler", icon="info", parent=self.listbox.master, type="ok", message="PDF file generated successfully.")
+                msgbox.show()
+            else:
+                msgbox = tkMessageBox.Message(title="gTTFSampler", icon="error", parent=self.listbox.master, type="ok", message="Error: %s" % (self.error,))
+                msgbox.show()
+        else:
+            # process the queue in the GUI thread
+            self.listbox.master.after(100, self.process_queue)
 
 class MainWindow(T.Frame):
     def __init__(self, master=None):
@@ -43,6 +135,53 @@ class MainWindow(T.Frame):
         self.widgets['font_selector'] = pack_widget(MainWindow_FontSelector(self), fill="both", expand=True)
         self.widgets['options_selector'] = pack_widget(MainWindow_OptionsSelector(self), fill="x")
         self.widgets['button_savePDF'] = pack_widget(T.Button(self, text="Save PDF..."))
+
+        self.widgets['button_savePDF']['command'] = self.button_savePDF_click
+
+    def button_savePDF_click(self):
+        global ttfsampler
+
+        try:
+            import reportlab
+        except ImportError:
+            msgbox = tkMessageBox.Message(title="gTTFSampler Error", icon="error", parent=self, type="ok", message="The ReportLab Toolkit is not installed.")
+            msgbox.show()
+            return
+
+        font_filenames = self.widgets['font_selector'].get_filenames()
+        if not font_filenames:
+            msgbox = tkMessageBox.Message(title="gTTFSampler Error", icon="error", parent=self, type="ok", message="No font(s) selected.")
+            msgbox.show()
+            return
+
+        options = self.widgets['options_selector'].get_options()
+
+        import ttfsampler
+
+        dialog = tkFileDialog.SaveAs(self, title="Save PDF...", defaultextension='.pdf', filetypes=[('PDF file', '*.pdf')])
+        output_filename = dialog.show()
+        if not output_filename:   # User pressed "Cancel"
+            return
+
+        cfg = ttfsampler.Config()
+        cfg.verbosity = 1
+        cfg.font_size = float(options['fontSize'])
+        cfg.allow_broken_fonts = options['ignoreBad']
+        cfg.specified_text = options['specifyText']
+        cfg.input_filenames = font_filenames
+        cfg.output_filename = output_filename
+
+        logwindow = T.Toplevel()
+        logwindow.title("ttfsampler output")
+        logwindow.geometry("600x300")
+        logwindow.minsize(100, 100)
+        logwidget = ScrolledListbox(logwindow)
+        logwidget.pack(expand=True, fill="both")
+
+        t = BatchThread(cfg, logwidget)
+        t.process_queue()
+        t.start()
+
 
 class MainWindow_FontSelector(T.LabelFrame):
     def __init__(self, master):
@@ -56,7 +195,7 @@ class MainWindow_FontSelector(T.LabelFrame):
         self.widgets['button_removeSelected'] = pack_widget(T.Button(f, text="Remove selected"), side="left", expand=True, fill="x")
 
         # List
-        self.widgets['listbox'] = pack_widget(T.Listbox(self, selectmode="extended"), fill="both", expand=True)
+        self.widgets['listbox'] = pack_widget(ScrolledListbox(self), fill="both", expand=True)
 
         # Add event handlers
         self.widgets['button_addFile']['command'] = self.button_addFile_click
@@ -108,23 +247,58 @@ class MainWindow_FontSelector(T.LabelFrame):
         for idx in indices:
             lb.delete(idx)
 
+    def get_filenames(self):
+        return self.widgets['listbox'].get(0, "end")
+
+class ScrolledListbox(T.Frame):
+    def __init__(self, master):
+        T.Frame.__init__(self, master)
+        self.widgets = {}
+
+        self.widgets['listbox'] = pack_widget(T.Listbox(self, selectmode="extended"), side="left", expand=True, fill="both")
+        self.widgets['scrollbar'] = pack_widget(T.Scrollbar(self), side="right", fill="y")
+
+        # Connect the listbox to the scrollbar
+        self.widgets['listbox']['yscrollcommand'] = self.widgets['scrollbar'].set
+        self.widgets['scrollbar']['command'] = self.widgets['listbox'].yview
+
+        # Proxy listbox methods
+        for name in ('curselection', 'delete', 'get', 'index', 'insert', 'itemconfigure', 'yview'):
+            setattr(self, name, getattr(self.widgets['listbox'], name))
+
 class MainWindow_OptionsSelector(T.LabelFrame):
     def __init__(self, master):
         T.LabelFrame.__init__(self, master, text="Options")
         self.widgets = {}
+        self.vars = {
+            'fontSize': T.IntVar(value=12),
+            'ignoreBad': T.BooleanVar(value=True),
+            'specifyText_check': T.BooleanVar(value=False),
+            'specifyText_text': T.StringVar(value="The quick brown fox jumps over the lazy dog."),
+        }
 
         # Font size
         f = pack_widget(T.Frame(self), anchor="w")
         pack_widget(T.Label(f, text="Font size:", justify="left"), side="left")
-        self.widgets['spinbox_fontSize'] = pack_widget(T.Spinbox(f, from_=1, to=100), side="left")
+        self.widgets['spinbox_fontSize'] = pack_widget(T.Spinbox(f, from_=1, to=100, textvariable=self.vars['fontSize']), side="left")
 
         # Ignore broken/duplicate fonts
-        self.widgets['check_ignoreBad'] = pack_widget(T.Checkbutton(self, text="Ignore broken/duplicate fonts", justify="left"), anchor="w")
+        self.widgets['check_ignoreBad'] = pack_widget(T.Checkbutton(self, text="Ignore broken/duplicate fonts", justify="left", variable=self.vars['ignoreBad']), anchor="w")
 
         # Specify text
         f = pack_widget(T.Frame(self), anchor="w", fill="x")
-        self.widgets['check_specifyText'] = pack_widget(T.Checkbutton(f, text="Specify text:", justify="left"), side="left")
-        self.widgets['text_specifyText'] = pack_widget(T.Entry(f), side="left", expand=True, fill="x")
+        self.widgets['check_specifyText'] = pack_widget(T.Checkbutton(f, text="Specify text:", justify="left", variable=self.vars['specifyText_check']), side="left")
+        self.widgets['text_specifyText'] = pack_widget(T.Entry(f, textvariable=self.vars['specifyText_text']), side="left", expand=True, fill="x")
+
+    def get_options(self):
+        retval = {}
+        retval['fontSize'] = self.vars['fontSize'].get()
+        retval['ignoreBad'] = self.vars['ignoreBad'].get()
+        if self.vars['specifyText_check'].get():
+            retval['specifyText'] = self.vars['specifyText_text'].get()
+        else:
+            retval['specifyText'] = None
+        return retval
 
 if __name__ == '__main__':
     #root = T.Tk()
